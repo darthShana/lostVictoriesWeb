83c3f0c65a8a34ee8e286a50dfd6501ed178e7fd
darthShana <darthShana@gmail.com>
2016-09-11 09:24:09 +1200

    public html

--- app/components/Parent.js
+++ app/components/Parent.js
@@ -8,9 +8,32 @@ var Parent = React.createClass({
   render: function(){
     return (
       <div>
-        <LoginModalLink modelTtile="Login / Register">
-          <LoginCreateUser />
-        </LoginModalLink>
+        <header class="container_12">
+              <div class="grid_1"></div>
+              <div class="grid_10 bold-header">
+                  <h3>war extra</h3>
+              </div>
+              <div class="login grid_1">
+                <LoginModalLink modelTtile="Login / Register">
+                  <LoginCreateUser />
+                </LoginModalLink>
+              </div>
+              <div class="grid_12 border newspaper-name">
+                  <h4>The Lost Victories Examiner</h4>
+              </div>
+      		<div class="grid_12 border details-outter">
+                  <div class="grid_6">
+                      <p>Volume VXII -- NO. 190</p>
+                  </div>
+                  <div class="grid_6 right-align">
+                      <p>March 12th, 1939</p>
+                  </div>
+      		</div>
+              <div class="grid_12 border title">
+              	<h1>Britain and France at War with Germany</h1>
+      		</div>
+      	</header>
+
 
         <GamesPanel />
         <div> This is the parent .. 46 </div>
